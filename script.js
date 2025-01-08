2345be1104caaa17de9576f9f1cfc45a2c851ac7
xCatGuy <catguytv@gmail.com>
2025-01-08 10:03:04 -0600

    Update script.js

--- script.js
+++ script.js
@@ -53,11 +53,11 @@ async function submitDonationForm(event) {
     "processed-quantity[]": formData.getAll('processed-quantity[]'),
   };
 
-  console.log("Submitting data to:", "https://script.google.com/macros/s/AKfycbwEjB3GM5EC8UMzkaPnmew7WZpaMsois8eIYbBuYAUk29uf9blT4jvYZqv8WJl65iyZfg/exec");
+  console.log("Submitting data to:", "https://script.google.com/macros/s/AKfycbwlXqIGqW2nYAt01JPq-Tti0xX7UU9JMj_-CTlVeH4F_AJMRCtUGQ8sXA0PytIlqcrqwQ/exec");
   console.log("Data being sent:", JSON.stringify(data));
 
   try {
-    const response = await fetch("https://script.google.com/macros/s/AKfycbwEjB3GM5EC8UMzkaPnmew7WZpaMsois8eIYbBuYAUk29uf9blT4jvYZqv8WJl65iyZfg/exec", {
+    const response = await fetch("https://script.google.com/macros/s/AKfycbwlXqIGqW2nYAt01JPq-Tti0xX7UU9JMj_-CTlVeH4F_AJMRCtUGQ8sXA0PytIlqcrqwQ/exec", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
